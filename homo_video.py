5e531e0d596fa9dcb61b011eee1bd6cf2b15ab2a
hailiang194 <hailiang194>
2021-03-09 17:39:32 +0700

    Change buffer and choose dst

--- homo_video.py
+++ homo_video.py
@@ -10,7 +10,7 @@ def pre_process(frame):
 
     return process_frame
 
-def is_valid_homography(dst, image):
+def is_valid_homography(dst, image, MIN_EDGE_RATIO=0.4):
     edge = np.float32([
         dst[1][0] - dst[0][0],
         dst[2][0] - dst[1][0],
@@ -19,7 +19,7 @@ def is_valid_homography(dst, image):
     ])
 
     edge_len = np.linalg.norm(edge, axis=1)
-    if abs((np.amax(edge_len) / np.amin(edge_len)) - (max(image.shape[:2]) / min(image.shape[:2]))) >= 0.4:
+    if abs((np.amax(edge_len) / np.amin(edge_len)) - (max(image.shape[:2]) / min(image.shape[:2]))) >= MIN_EDGE_RATIO:
         return False
 
     get_cos = lambda x, y: (np.dot(x, y)) / (np.linalg.norm(x) * np.linalg.norm(y))
@@ -42,12 +42,13 @@ def is_valid_homography(dst, image):
     return True
 
 
-def get_homography(frame, query_pts, train_pts, image):
-
-    currentPts = len(query_pts)
-    if query_pts.shape[0] <= 25:
+def get_homography(frame, query_pts, train_pts, image, LIMIT_POINTS_FOR_MIDDLE=25, REMAP_ACCEPTED_DISTANCE=5.0, MIN_EDGE_RATIO=0.4, MIN_POINTS=10):
+    
+    #add middle points for pairs of points which is too far
+    len_points = len(query_pts)
+    if query_pts.shape[0] <= LIMIT_POINTS_FOR_MIDDLE:
         MIN_DISTANCE_OF_MIDPOINT = min(frame.shape[0] * 0.05, frame.shape[1] * 0.05)
-        for i in range(1, currentPts):
+        for i in range(1, len_points):
             for j in range(i):
                 if np.linalg.norm(train_pts[i] - train_pts[j]) > MIN_DISTANCE_OF_MIDPOINT:
                     query_pts = np.append(query_pts, [(query_pts[i] + query_pts[j]) / 2.0], axis=0)
@@ -67,13 +68,13 @@ def get_homography(frame, query_pts, train_pts, image):
 
     for i in range(len(query_pts)):
         distance = np.linalg.norm(real_train_pts[i][0] - train_pts[i][0])
-        if distance <=  5.0:
+        if distance <= REMAP_ACCEPTED_DISTANCE:
             distance_filtered_query_pts.append(query_pts[i])
             distance_filtered_train_pts.append(train_pts[i])
 
     query_pts = np.float32(distance_filtered_query_pts)
     train_pts = np.float32(distance_filtered_train_pts)
-    if len(good_points) > 10 and ((query_pts.shape[0]) >= 4): 
+    if len(good_points) > MIN_POINTS and ((query_pts.shape[0]) >= 4): 
         mat, mask = cv2.findHomography(query_pts, train_pts, cv2.RANSAC, 5.0)
         if mat is None:
             return query_pts.reshape(-1, 2), train_pts.reshape(-1, 2), None
@@ -85,7 +86,7 @@ def get_homography(frame, query_pts, train_pts, image):
 
         dst = cv2.perspectiveTransform(pts, mat)
 
-        if is_valid_homography(dst, image):
+        if is_valid_homography(dst, image, MIN_EDGE_RATIO):
             return query_pts.reshape(-1, 2), train_pts.reshape(-1, 2), dst
     return query_pts.reshape(-1, 2), train_pts.reshape(-1, 2), None
 
@@ -98,14 +99,15 @@ if __name__ == "__main__":
     bf = cv2.BFMatcher(cv2.NORM_HAMMING)
     kp_image, desc_image = orb.detectAndCompute(img, None)
     lk_params = dict(winSize=(25, 25), 
-                     maxLevel = 9,
+                     maxLevel = 6,
                      criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
     old_frame = None
     old_points = None
     query_points = None
     dst = None
 
-    FRAMES_BUFFER = 20
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    FRAMES_BUFFER = 4 * fps
     
     buffer = 0
 
@@ -158,18 +160,22 @@ if __name__ == "__main__":
             for i in range(old_points.shape[0]):
                 cv2.line(frame, (int(old_points[i, 0]), int(old_points[i, 1])), (int(new_points[i, 0]), int(new_points[i, 1])), (0, 255, 0), 3)
             dst = cv2.perspectiveTransform(pts, mat)
+            if dst is not None and not is_valid_homography(dst, img):
+                query_points, new_points, dst = get_homography(frame, query_points.reshape(-1, 1, 2), new_points.reshape(-1, 1, 2), img)
             old_frame = process_frame
             old_points = new_points
             buffer = buffer + 1
 
            
-
-        if dst is None or not is_valid_homography(dst, img):
-            cv2.imshow("frame", frame)
+        if dst is None:
+            # cv2.imshow("frame", frame)
             buffer = FRAMES_BUFFER
-        else:
-            test = cv2.polylines(frame, [np.int32(dst)], True, (255, 0, 0), 3)
-            cv2.imshow("frame", test)
+        if not dst is None:
+            frame = cv2.polylines(frame, [np.int32(dst)], True, (255, 0, 0), 3)
+            # cv2.imshow("frame", test)
+        
+        cv2.imshow("frame", frame)
+
 
         key = cv2.waitKey(1)
         if key == 27:
